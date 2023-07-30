f1c8234dba27006c6bb873dfce1f64a869c41dd6
Luis Martinez <rust@Luiss-Mac-mini.local>
2023-07-29 23:49:24 -0600

    DB configuration, creation of models Table and Menu.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Table(models.Model):
+    name = models.CharField(max_length=255)
+    no_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
+
+    def __str__(self) -> str:
+        return f'{self.title}: {self.price}'
